53c2b8f1ff98084e9bde104582e1b2b42209c9a3
SaamVR <163770486+SaamVR@users.noreply.github.com>
2026-09-25 16:04:59 +0600

    feat: wire EZStay scheduled runtime entrypoint

--- cloudflare/ezstay-runtime/worker.js
+++ cloudflare/ezstay-runtime/worker.js
@@ -1,5 +1,6 @@
 import { createBackendAdapter } from "./backend.js";
 import { routeEzstayRuntimeRequest } from "./router.js";
+import { runScheduledTick } from "./scheduler.js";
 
 export default {
   async fetch(request, env) {
@@ -7,5 +8,14 @@ export default {
       request,
       backend:createBackendAdapter(env),
     });
+  },
+
+  scheduled(controller, env, ctx) {
+    const promise = runScheduledTick(env, createBackendAdapter(env));
+    if (ctx?.waitUntil) {
+      ctx.waitUntil(promise);
+      return;
+    }
+    return promise;
   },
 };
