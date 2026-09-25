c6d7ed886644958ad3da7dcca8a53beb67635dda
SaamVR <163770486+SaamVR@users.noreply.github.com>
2026-09-25 16:16:52 +0600

    feat: require anonymous identity for public demo creation

--- functions/api/ezstay/demo/start.js
+++ functions/api/ezstay/demo/start.js
@@ -1,5 +1,5 @@
 import { delegateEzstayRequest } from "../../../../_shared/ezstay/gateway.js";
 
 export function onRequestPost({ request, env }) {
-  return delegateEzstayRequest({ request, env, mutation:true });
+  return delegateEzstayRequest({ request, env, mutation:true, anonymousOnly:true });
 }
